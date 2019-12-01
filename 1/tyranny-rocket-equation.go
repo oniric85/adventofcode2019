3d97ef3b368f6f715448f8898f555784c02f4493
Stefano Angaran <stefano.angaran@upyou.it>
2019-12-01 21:49:30 +0100

    Solved day 1 part 2

--- 1/tyranny-rocket-equation.go
+++ 1/tyranny-rocket-equation.go
@@ -7,17 +7,44 @@ import (
 	"strings"
 )
 
-func solve1(input string) int {
-	masses := strings.Split(input, "\n")
+func calculateRequiredFuel(mass int) int {
+	return mass/3 - 2
+}
 
+func calculateAdjustedRequiredFuel(mass int) int {
+	fuel := calculateRequiredFuel(mass)
+
+	if fuel <= 0 {
+		return 0
+	}
+
+	return fuel + calculateAdjustedRequiredFuel(fuel)
+}
+
+func solve1(masses []string) int {
 	totalFuelRequirement := 0
 	for _, massString := range masses {
 		massInt, err := strconv.Atoi(massString)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fuel := massInt/3 - 2
+		fuel := calculateRequiredFuel(massInt)
 		log.Printf("Fuel needed for module with weight %d is %d", massInt, fuel)
+		totalFuelRequirement += fuel
+	}
+
+	return totalFuelRequirement
+}
+
+func solve2(masses []string) int {
+	totalFuelRequirement := 0
+	for _, massString := range masses {
+		massInt, err := strconv.Atoi(massString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fuel := calculateAdjustedRequiredFuel(massInt)
+		log.Printf("Adjusted fuel needed for module with weight %d is %d", massInt, fuel)
 		totalFuelRequirement += fuel
 	}
 
@@ -30,7 +57,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	result := solve1(string(lines))
+	input := strings.Split(string(lines), "\n")
+
+	result := solve1(input)
+	adjustedResult := solve2(input)
 
 	log.Println("Total fuel requirement is", result)
+	log.Println("Total adjusted fuel requirement is", adjustedResult)
 }
